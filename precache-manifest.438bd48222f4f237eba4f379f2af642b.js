450d45870d93a82f3a4fb6b8042c3b66ef29fdb6
bmalkmus <bmalkmus23@gmail.com>
2020-04-30 13:32:15 -0700

    Updates

---
+++ precache-manifest.438bd48222f4f237eba4f379f2af642b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83414b7c32111cabd8a41b555bf67bda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "290c27f5a7af862dce9c",
+    "url": "/static/css/main.db6983cb.chunk.css"
+  },
+  {
+    "revision": "5adf82fce925ab2a1598",
+    "url": "/static/js/2.b2b01ba5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b2b01ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290c27f5a7af862dce9c",
+    "url": "/static/js/main.3bfe748c.chunk.js"
+  },
+  {
+    "revision": "12371047c58a498f4779",
+    "url": "/static/js/runtime-main.2101719f.js"
+  },
+  {
+    "revision": "9309b3a782cff04197dc9cea799599ed",
+    "url": "/static/media/Resume.9309b3a7.pdf"
+  },
+  {
+    "revision": "14597274b831afa88c2e0c66c070d960",
+    "url": "/static/media/profilePicture.14597274.jpg"
+  },
+  {
+    "revision": "6e89e277e0ae0de3b0e97cbff50de188",
+    "url": "/static/media/underwater.6e89e277.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.6a70a78736d4af1cd5390199cb5a1b99.js"
+  "/precache-manifest.438bd48222f4f237eba4f379f2af642b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
